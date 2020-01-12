89fd2c92b16aa91c3195d67389413c1848e514bb
Jeff Dam <jeff.dam@gmail.com>
2020-01-12 15:36:21 -0800

    Edit task info display text and styling

--- src/components/task/task_display/task_info.jsx
+++ src/components/task/task_display/task_info.jsx
@@ -7,13 +7,13 @@ const TaskInfo = ({ props: { task, deleteTask, changeStatus }, showForm}) => {
   const deleteButton = <i className="fas fa-trash-alt" onClick={deleteTask} title="Delete Task"></i>;
   const showMoveUpButton = task.status === "completed" ? "" : moveUpButton;
   const showMoveDownButton = task.status === "backlog" ? "" : moveDownButton;
-  const dueDate = new Date(task.dueDate).toLocaleDateString("en-US", { year: 'numeric', month: '2-digit', day: '2-digit' })
+  const dueDate = task.dueDate.length ? new Date(task.dueDate).toLocaleDateString("en-US", { year: 'numeric', month: '2-digit', day: '2-digit' }) : "";
   return (
     <ul>
-      <li>TASK: {task.title}</li>
-      <li>Due by: {dueDate}</li>
-      <li>Created on: {task.createDate}</li>
+      <li>Task: {task.title}</li>
       <li>Notes: {task.notes}</li>
+      <li>Created: {task.createDate}</li>
+      <li>Due: {dueDate}</li>
       <li className="task-buttons">
         {editFormButton}
         {deleteButton}
