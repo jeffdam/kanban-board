059d9a7a7c491eb402de3f634bf5bbfc00ee2d99
Jeff Dam <jeff.dam@gmail.com>
2020-01-12 15:41:01 -0800

    Updates

---
+++ precache-manifest.27709de2959e19df35b1ed598d46817f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d1c26d5f11f4cda1a5766b123819dde",
+    "url": "/kanban-board/index.html"
+  },
+  {
+    "revision": "1cfc7c42d4c2e9af557d",
+    "url": "/kanban-board/static/css/main.25f57528.chunk.css"
+  },
+  {
+    "revision": "20ddd2c8e50e2f12cb2f",
+    "url": "/kanban-board/static/js/2.284ba0df.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/kanban-board/static/js/2.284ba0df.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1cfc7c42d4c2e9af557d",
+    "url": "/kanban-board/static/js/main.97ba96c7.chunk.js"
+  },
+  {
+    "revision": "431af74cddbe789ba0f7",
+    "url": "/kanban-board/static/js/runtime-main.e5d4e461.js"
+  }
+]);
