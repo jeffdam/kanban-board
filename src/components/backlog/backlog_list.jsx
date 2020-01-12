94ac072091b9ac7bea6a47fc950567b7fcaa01ea
Jeff Dam <jeff.dam@gmail.com>
2020-01-11 17:11:47 -0800

    Add task display

--- src/components/backlog/backlog_list.jsx
+++ src/components/backlog/backlog_list.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
 
-const BacklogList = props => {
-  console.log(props.tasks)
+const BacklogList = ({ tasks }) => {
+  const taskDisplayList = tasks.map(task => (
+    <ul key={task.id}>
+      <li>TASK: {task.title}</li>
+      <li>Due by: {task.dueDate}</li>
+      <li>Created on: {task.createDate}</li>
+      <li>Description: {task.description}</li>
+    </ul>
+  ));
   return (
-    <h2>BACKLOG</h2>
+    <article>
+      <h2>BACKLOG</h2>
+      { taskDisplayList }
+    </article>
   )
 };
 
